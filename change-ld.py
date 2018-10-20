2de473cc3fe299bb570924dbade0e733f4808cb1
raywang <raywang@mit.edu>
2018-10-19 23:08:21 -0400

    Added change-ld

---
+++ change-ld.py
@@ -0,0 +1,43 @@
+from pwn import *
+import sys
+
+def change_ld(binary, ld):
+    """
+    Force to use assigned new ld.so by changing the binary
+    """
+    if not os.access(ld, os.R_OK): 
+        log.failure("Invalid path {} to ld".format(ld))
+        return None
+
+        
+    if not isinstance(binary, ELF):
+        if not os.access(binary, os.R_OK): 
+            log.failure("Invalid path {} to binary".format(binary))
+            return None
+        binary = ELF(binary)
+
+    for segment in binary.segments:
+        if segment.header['p_type'] == 'PT_INTERP':
+            size = segment.header['p_memsz']
+            addr = segment.header['p_paddr']
+            data = segment.data()
+            if size <= len(ld):
+                log.failure("Failed to change PT_INTERP from {} to {}".format(data, ld))
+                return None
+            binary.write(addr, ld.ljust(size, '\0'))
+            if not os.access('/tmp/pwn', os.F_OK): os.mkdir('/tmp/pwn')
+            path = '/tmp/pwn/{}_debug'.format(os.path.basename(binary.path))
+            if os.access(path, os.F_OK): 
+                os.remove(path)
+                info("Removing exist file {}".format(path))
+            binary.save(path)    
+            os.chmod(path, 0b111000000) #rwx------
+    log.success("PT_INTERP has changed from {} to {}. Using temp file {}".format(data, ld, path)) 
+    return ELF(path)
+
+
+if len(sys.argv) < 3:
+    print "change_ld.py <binary> <ld>"
+    exit()
+
+change_ld(sys.argv[1], sys.argv[2])
